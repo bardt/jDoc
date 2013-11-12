02f8918c277165e6cd32854895cb5bc1b6a6c4a1
webschik <webschik@gmail.com>
2013-11-12 21:11:48 +0200

    bug fix

--- src/build/jdoc.0.1.0.js
+++ src/build/jdoc.0.1.0.js
@@ -9542,7 +9542,9 @@
                         parseParams.braceCounter -= 1;
                         i += 1;
                         if (parseParams.currentTextElement && parseParams.currentTextElement.properties.textContent.length) {
-                            parseParams.currentTextElementParent.elements.push(parseParams.currentTextElement);
+                            if (parseParams.currentTextElementParent.elements.indexOf(parseParams.currentTextElement) < 0) {
+                                parseParams.currentTextElementParent.elements.push(parseParams.currentTextElement);
+                            }
                             parseParams.currentTextElement = {
                                 options: {},
                                 css: {},
